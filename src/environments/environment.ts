d03ee2b830da7040c5c0d5a971e48841a5d6a819
Sherly Febrianti <sherly.febrianti@allianz.co.id>
2019-11-20 14:59:48 +0700

    Prepare the default case

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  api: 'https://example.com/api/default',
+  payment: 'https://example.com/payment/default'
 };
 
 /*
